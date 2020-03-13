7042b91684c6a124ef1c6bc4bee3616c61fcc8ea
cbizon <bizon@renci.org>
2020-03-13 13:51:18 -0400

    Fixes Travis, one test working

--- tests/test_service.py
+++ tests/test_service.py
@@ -3,7 +3,16 @@ from fastapi.testclient import TestClient
 
 def test_simple_check():
     client = TestClient(app)
-    params = {'string':'diabetes'}
+    params = {'string':'alzheimer'}
     response = client.post("/lookup",params=params)
-    print(response.json())
-    assert False
+    syns = response.json()
+    #There are more than 10, but it should cut off at 10 if we don't give it a max?
+    assert len(syns) == 10
+
+def xtest_max():
+    client = TestClient(app)
+    params = {'string': 'alzheimer'}
+    response = client.post("/lookup", params=params)
+    syns = response.json()
+    # There are more than 10, but it should cut off at 10 if we don't give it a max?
+    assert len(syns) == 10
