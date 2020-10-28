480ad605c2fe95695af33644c94129cad7e070a0
Patrick Wang <patrick@covar.com>
2020-10-27 21:33:54 -0400

    Use double quotes

--- api/server.py
+++ api/server.py
@@ -19,41 +19,41 @@ from pydantic import conint
 from starlette.middleware.cors import CORSMiddleware
 
 LOGGER = logging.getLogger(__name__)
-SOLR_HOST = os.getenv('SOLR_HOST', 'localhost')
-SOLR_PORT = os.getenv('SOLR_PORT', '8983')
+SOLR_HOST = os.getenv("SOLR_HOST", "localhost")
+SOLR_PORT = os.getenv("SOLR_PORT", "8983")
 
 app = FastAPI(
-    title='Name Resolver',
-    description='Biomedical entity name resolution service',
-    version='1.0.0',
+    title="Name Resolver",
+    description="Biomedical entity name resolution service",
+    version="1.0.0",
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.post('/lookup', response_model=Dict[str, List[str]], tags=['lookup'])
+@app.post("/lookup", response_model=Dict[str, List[str]], tags=["lookup"])
 async def lookup_curies(
         string: str,
         offset: int = 0,
         limit: conint(le=1000) = 10,
 ) -> Dict[str, List[str]]:
     """Look up curies from name or fragment."""
-    fragments = string.split(' ')
-    name_filters = ' AND '.join(
-        f'name:/.*{re.escape(fragment)}.*/'
+    fragments = string.split(" ")
+    name_filters = " AND ".join(
+        f"name:/.*{re.escape(fragment)}.*/"
         for fragment in fragments
     )
-    query = f'http://{SOLR_HOST}:{SOLR_PORT}/solr/name_lookup/select'
+    query = f"http://{SOLR_HOST}:{SOLR_PORT}/solr/name_lookup/select"
     params = {
-        'query': name_filters,
-        'limit': 0,
-        'sort': 'length ASC',
-        'facet': {
+        "query": name_filters,
+        "limit": 0,
+        "sort": "length ASC",
+        "facet": {
             "categories": {
                 "type": "terms",
                 "field": "curie",
@@ -71,35 +71,35 @@ async def lookup_curies(
         response = await client.post(query, json=params)
     response.raise_for_status()
     response = response.json()
-    if not response['response']['numFound']:
+    if not response["response"]["numFound"]:
         return dict()
-    buckets = response['facets']['categories']['buckets']
+    buckets = response["facets"]["categories"]["buckets"]
 
     curie_filter = " OR ".join(
         f"curie:/{re.escape(bucket['val'])}/"
         for bucket in buckets
     )
     params = {
-        'query': f"({curie_filter}) AND ({name_filters})",
-        'limit': 1000000,
-        'sort': 'length ASC',
-        'fields': 'curie,name',
+        "query": f"({curie_filter}) AND ({name_filters})",
+        "limit": 1000000,
+        "sort": "length ASC",
+        "fields": "curie,name",
     }
     async with httpx.AsyncClient(timeout=None) as client:
         response = await client.post(query, json=params)
     response.raise_for_status()
     output = defaultdict(list)
-    for doc in response.json()['response']['docs']:
+    for doc in response.json()["response"]["docs"]:
         output[doc["curie"]].append(doc["name"])
     params = {
-        'query': f"({curie_filter}) AND NOT ({name_filters})",
-        'limit': 1000000,
-        'sort': 'length ASC',
-        'fields': 'curie,name',
+        "query": f"({curie_filter}) AND NOT ({name_filters})",
+        "limit": 1000000,
+        "sort": "length ASC",
+        "fields": "curie,name",
     }
     async with httpx.AsyncClient(timeout=None) as client:
         response = await client.post(query, json=params)
     response.raise_for_status()
-    for doc in response.json()['response']['docs']:
+    for doc in response.json()["response"]["docs"]:
         output[doc["curie"]].append(doc["name"])
     return output
