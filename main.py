b9f624e12f5d8faab493f909a8f92084bd3e752f
Phil Owen <phillipsowen@outlook.com>
2020-11-03 16:03:25 -0500

    fixing comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from gunicorn.app.wsgiapp import WSGIApplication
 
-# --bind 0.0.0.0:8080 -w 1 -k uvicorn.workers.UvicornWorker -t 600 ranker.server:APP
+# --bind 0.0.0.0:8080 -w 1 -k uvicorn.workers.UvicornWorker -t 600 api.server:app
 app = WSGIApplication()
 
 app.run()
