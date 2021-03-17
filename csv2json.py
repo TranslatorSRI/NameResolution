839306949565fc27d64d91f5032417e6f62aa668
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-03-17 15:56:10 -0400

    Tweak file reformatting

--- csv2json.py
+++ csv2json.py
@@ -4,23 +4,28 @@ import argparse
 import csv
 import json
 
+from tqdm import tqdm
+
 
 def reformat(infile: str, outfile: str, separator: str = ","):
     """Reformat name file."""
-    output = []
     with open(infile, "r") as f:
         reader = csv.reader(f, delimiter=separator, quotechar="\"")
-        for idx, row in enumerate(reader):
-            entity_id = ":".join(row[0].split("/")[-1].split("_"))
-            name = row[-1]
-            output.append({
-                "id": idx,
-                "curie": entity_id,
-                "name": name,
-                "length": len(name)
-            })
-    with open(outfile, "w") as f:
-        json.dump(output, f)
+        with open(outfile, "w") as f:
+            f.write("[\n")
+            for idx, row in tqdm(enumerate(reader)):
+                entity_id = ":".join(row[0].split("/")[-1].split("_"))
+                name = row[-1]
+                if idx:
+                    f.write(",\n")
+                f.write("\t")
+                json.dump({
+                    "id": idx,
+                    "curie": entity_id,
+                    "name": name,
+                    "length": len(name)
+                }, f)
+            f.write("\n]")
 
 
 if __name__ == "__main__":
