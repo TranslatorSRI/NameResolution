5ef87f818951087b654af13639bd28f897631636
Gaurav Vaidya <gaurav@renci.org>
2024-02-27 10:08:41 -0500

    Tweaked boosts.

--- api/server.py
+++ api/server.py
@@ -360,10 +360,10 @@ async def lookup(string: str,
                 "query": query,
                 # qf = query fields, i.e. how should we boost these fields if they contain the same fields as the input.
                 # https://solr.apache.org/guide/solr/latest/query-guide/dismax-query-parser.html#qf-query-fields-parameter
-                "qf": "preferred_name_exactish^50 preferred_name^20 names^1",
+                "qf": "preferred_name_exactish^30 preferred_name^20 names^10",
                 # pf = phrase fields, i.e. how should we boost these fields if they contain the entire search phrase.
                 # https://solr.apache.org/guide/solr/latest/query-guide/dismax-query-parser.html#pf-phrase-fields-parameter
-                "pf": "preferred_name_exactish^60 preferred_name^30 names^10",
+                "pf": "preferred_name_exactish^35 preferred_name^25 names^15",
                 # Boost by:
                 "bq":   boost_queries,
             },
